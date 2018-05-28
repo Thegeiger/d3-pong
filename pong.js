1db06671eb9ba03d4c32426e2923df458daac584
Thegeiger <anthony.geiger@epitech.eu>
2018-05-28 23:32:24 +0200

    setup

---
+++ pong.js
@@ -0,0 +1,2 @@
+let svgContainer = d3.select("#pong").append("svg").attr("width", "98%").attr("height", "98vh").style("margin-top",  "1%").style("margin-left",  "1%").append("g").attr("stroke", "black").attr("stroke-width", "10px");
+svgContainer.append("rect").attr("x", 0).attr("y", 0).attr("height", "10px").attr("width", "20%");
